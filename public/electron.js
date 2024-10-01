86eb04bb742e040dc54a814110cfc423e043f498
Amir <amirtashdid99@gmail.com>
2024-10-02 01:08:58 +0330

    idk

--- public/electron.js
+++ public/electron.js
@@ -1,33 +1,38 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const isDev = require("electron-is-dev");
+const url = require("url");
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../build/index.html"),
+      protocol: "file:",
+      slashes: true,
     });
 
-    win.loadURL(
-        isDev ?
-        "http://localhost:3000" :
-        `file://${path.join(__dirname, "../build/index.html")}`
-    );
+  win.loadURL(startUrl);
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
